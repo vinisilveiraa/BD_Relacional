581692ddf47ee929e2071e1bf050fbd4e9560a55
Vinicius Leonardo Silveira <vinicius.silveira9@fatec.sp.gov.br>
2025-11-13 12:34:45 -0300

    aula 13-11

--- sakila_ptbr/use_sakila_pt-br.sql
+++ sakila_ptbr/use_sakila_pt-br.sql
@@ -25,3 +25,44 @@ FROM pagamentos
 INNER JOIN clientes ON pagamentos.cliente_id = clientes.id_cliente;
 -- "Junte as linhas das duas tabelas onde o cliente_id da tabela de pagamentos for igual ao id_cliente da tabela de clientes."
 
+-- SUBQUERY - Podemos inserir SELECTs, dentro de outros SELECTs
+-- para ampliar o poder de consulta em lugares que o JOIN não resolve
+
+
+
+-- exibir clientes com pagamentos acima da media
+SELECT 
+	-- Concatena primeiro e último nome do cliente em uma única coluna
+	CONCAT(c.primeiro_nome, ' ', c.ultimo_nome) AS nome_cliente,
+    -- Mostra a data do pagamento
+    p.data_pagamento,
+    -- Mostra o valor do pagamento
+    p.valor
+FROM clientes c 
+-- Faz um INNER JOIN para relacionar cada pagamento ao respectivo cliente
+INNER JOIN pagamentos p ON c.id_cliente = p.cliente_id
+WHERE 
+	-- Filtra apenas pagamentos cujo valor seja MAIOR do que a média geral
+	p.valor > (
+        -- Subconsulta: calcula a média de todos os pagamentos registrados
+        SELECT AVG(valor)
+        FROM pagamentos
+    );
+    
+-- select dentro do from - usamos o resultado de um select para fazer novos filtros.ALTER
+SELECT resultado.nome_cliente, resultado_valor 
+FROM (
+	SELECT 	CONCAT(c.primeiro_nome, ' ', c.ultimo_nome) AS nome_cliente,
+		p.data_pagamento,
+		p.valor
+	FROM clientes c 
+	INNER JOIN pagamentos p ON c.id_cliente = p.cliente_id
+	WHERE 
+		p.valor > (
+			SELECT AVG(valor)
+			FROM pagamentos
+		) -- fim do select interno
+	) AS resultado
+WHERE resultado.valor < 6;
+
+-- select dentro do select dentro do select
